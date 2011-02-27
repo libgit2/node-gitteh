8469b0411e6c439938531d4813fdd070db512f3b
Sam <sam.c.day@gmail.com>
2011-02-27 22:00:08 +1000

    Object graphs SUCK

--- src/libgit2_bindings.cc
+++ src/libgit2_bindings.cc
@@ -8,7 +8,14 @@
 
 using namespace std;
 using namespace v8;
-using namespace node;
+using namespace node;  
+
+static void StubWeakCallback(Persistent<Value> val, void* data) {
+	// TODO: should we actually be doing anything here? node's ObjectWrap deletes the object, but as far as I can tell, the dtor
+	// for my weak ref'd stuff is getting called, which means memory is being freed... Unless I'm completely retarded? 
+}
+
+#define REPO_INTERNAL_FIELD_COMMIT_STORE 1
 
 #define RAWOBJ_TYPE_SYMBOL String::NewSymbol("type")
 #define RAWOBJ_DATA_SYMBOL String::NewSymbol("data")
@@ -209,7 +216,7 @@ public:
 protected:
 	static Handle<Value> New(const Arguments& args) {
 		HandleScope scope;
-	
+
 		REQ_ARGS(1);
 		REQ_EXT_ARG(0, theCommit);
 	
@@ -290,6 +297,9 @@ protected:
 		Local<Value> arg = External::New((void*)tree);
 		Persistent<Object> result(Tree::constructor_template->GetFunction()->NewInstance(1, &arg));
 		return scope.Close(result);
+	}
+	
+	~Commit() {
 	}
 
 	git_commit *commit_;
@@ -408,7 +418,7 @@ public:
 
 		Local<FunctionTemplate> t = FunctionTemplate::New(New);
 		t->Inherit(EventEmitter::constructor_template);
-    	t->InstanceTemplate()->SetInternalFieldCount(1);
+    	t->InstanceTemplate()->SetInternalFieldCount(3);
 
     	NODE_SET_PROTOTYPE_METHOD(t, "getObjectDatabase", GetODB);
     	NODE_SET_PROTOTYPE_METHOD(t, "getCommit", GetCommit);
@@ -425,11 +435,16 @@ protected:
 
 		Repository *repo = new Repository();
 
-		if(int result = repo->open(*path) != GIT_SUCCESS) {
+		if(int result = git_repository_open(&repo->repo_, *path) != GIT_SUCCESS) {
 			Handle<Value> ex = Exception::Error(String::New("Git error."));
 			return ThrowException(ex);
 		}
 		
+		// Create our backing store.
+		Persistent<Object> commitStore = Persistent<Object>::New(Object::New());
+		commitStore.MakeWeak(&commitStore, StubWeakCallback);
+		args.This()->SetInternalField(REPO_INTERNAL_FIELD_COMMIT_STORE, commitStore);
+
 		repo->Wrap(args.This());
 		repo->MakeWeak();
 
@@ -440,7 +455,7 @@ protected:
 		HandleScope scope;
 
 		Repository *repo = ObjectWrap::Unwrap<Repository>(args.This());
-		git_odb *odb = repo->getOdb();
+		git_odb *odb = git_repository_database(repo->repo_);
 		
 		Local<Value> arg = External::New(odb);
 		Persistent<Object> result(ObjectDatabase::constructor_template->GetFunction()->NewInstance(1, &arg));
@@ -452,34 +467,38 @@ protected:
 		
 		REQ_ARGS(1);
 		REQ_OID_ARG(0, commitOid);
-
-		Repository *repo = ObjectWrap::Unwrap<Repository>(args.This());
 		
+		Repository *repo = ObjectWrap::Unwrap<Repository>(args.This());
+		Local<Object> commitStore = Local<Object>::Cast(args.This()->GetInternalField(REPO_INTERNAL_FIELD_COMMIT_STORE));
+		const char* oidStr = git_oid_allocfmt(&commitOid);	
+				
+		Local<Value> possibleCommit = commitStore->Get(String::New(oidStr));
+
+		if(!possibleCommit->IsUndefined()) {
+			return scope.Close(commitStore->Get(String::New(oidStr)));
+		}
+
 		git_commit* commit;
 		if(git_commit_lookup(&commit, repo->repo_, &commitOid) != GIT_SUCCESS) {
 			// TODO: error code handling.
 			return Null();
 		}
-		
+
 		Local<Value> arg = External::New(commit);
 		Persistent<Object> result(Commit::constructor_template->GetFunction()->NewInstance(1, &arg));
+		result.MakeWeak(&arg, StubWeakCallback);
+
+		commitStore->Set(String::NewSymbol(oidStr), result);
+		
 		return scope.Close(result);
 	}
 
 	Repository() : EventEmitter()  {
 		
 	}
-	
+
 	~Repository() {
 		close();
-	}
-
-	int open(char *path) {
-		return git_repository_open(&repo_, path);
-	}
-	
-	git_odb *getOdb() {
-		return git_repository_database(repo_);
 	}
 
 	void close() {
