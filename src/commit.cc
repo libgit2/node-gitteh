7d5a66fb696e6118824d16d50b43476fae61b948
Sam Day <sam.c.day@gmail.com>
2011-02-28 14:55:56 +1000

    Commit object no longer uses accessors for basic properties.

--- src/commit.cc
+++ src/commit.cc
@@ -10,13 +10,13 @@ void Commit::Init(Handle<Object> target) {
 	constructor_template->SetClassName(String::New("Commit"));
 	t->InstanceTemplate()->SetInternalFieldCount(1);
 
-	t->PrototypeTemplate()->SetAccessor(COMMIT_ID_SYMBOL, IdGetter);
+	/*t->PrototypeTemplate()->SetAccessor(COMMIT_ID_SYMBOL, IdGetter);
 	t->PrototypeTemplate()->SetAccessor(COMMIT_MESSAGE_SYMBOL, MessageGetter);
 	t->PrototypeTemplate()->SetAccessor(COMMIT_MESSAGE_SHORT_SYMBOL, MessageShortGetter);
 	t->PrototypeTemplate()->SetAccessor(COMMIT_TIME_SYMBOL, TimeGetter);
 	t->PrototypeTemplate()->SetAccessor(COMMIT_AUTHOR_SYMBOL, AuthorGetter);
 	t->PrototypeTemplate()->SetAccessor(COMMIT_COMMITTER_SYMBOL, CommitterGetter);
-	t->PrototypeTemplate()->SetAccessor(COMMIT_TREE_SYMBOL, TreeGetter);
+	t->PrototypeTemplate()->SetAccessor(COMMIT_TREE_SYMBOL, TreeGetter);*/
 }
 
 Handle<Value> Commit::New(const Arguments& args) {
@@ -30,70 +30,35 @@ Handle<Value> Commit::New(const Arguments& args) {
 	commit->commit_ = (git_commit*)theCommit->Value();
 	commit->repo_ = (Repository*)theRepo->Value();
 
+	// Setup some basic info about this commit.
+	const char* oidStr = git_oid_allocfmt(git_commit_id(commit->commit_));
+	args.This()->Set(String::New("id"), String::New(oidStr));
+
+	const char* message = git_commit_message(commit->commit_);
+	args.This()->Set(String::New("message"), String::New(message));
+
+	const char* shortMessage = git_commit_message_short(commit->commit_);
+	args.This()->Set(String::New("shortMessage"), String::New(shortMessage));
+
+	time_t time = git_commit_time(commit->commit_);
+	args.This()->Set(String::New("time"), Date::New(static_cast<double>(time)*1000));
+
+	const git_signature *author;
+	author = git_commit_author(commit->commit_);
+	CREATE_PERSON_OBJ(authorObj, author);
+	args.This()->Set(String::New("author"), authorObj);
+
+	const git_signature *committer;
+	committer = git_commit_committer(commit->commit_);
+	CREATE_PERSON_OBJ(committerObj, committer);
+	args.This()->Set(String::New("committer"), committerObj);
+
 	commit->Wrap(args.This());
 	return args.This();
 }
 
-Handle<Value> Commit::IdGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-		Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-		const char* oidStr = git_oid_allocfmt(git_commit_id(commit->commit_));
-
-		return scope.Close(String::New(oidStr));
-}
-
-Handle<Value> Commit::MessageGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-	const char* message = git_commit_message(commit->commit_);
-
-	return scope.Close(String::New(message));
-}
-
-Handle<Value> Commit::MessageShortGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-	const char* message = git_commit_message_short(commit->commit_);
-
-	return scope.Close(String::New(message));
-}
-
-Handle<Value> Commit::TimeGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-	time_t time = git_commit_time(commit->commit_);
-	return scope.Close(Date::New(static_cast<double>(time)*1000));
-}
-
-Handle<Value> Commit::AuthorGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-	const git_signature *author;
-	author = git_commit_author(commit->commit_);
-	CREATE_PERSON_OBJ(authorObj, author);
-
-	return scope.Close(authorObj);
-}
-
-Handle<Value> Commit::CommitterGetter(Local<String> property, const AccessorInfo& info) {
-	HandleScope scope;
-
-	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
-	const git_signature *committer;
-	committer = git_commit_committer(commit->commit_);
-	CREATE_PERSON_OBJ(committerObj, committer);
-
-	return scope.Close(committerObj);
-}
-
 Handle<Value> Commit::TreeGetter(Local<String> property, const AccessorInfo& info) {
 	HandleScope scope;
-
 
 	Commit *commit = ObjectWrap::Unwrap<Commit>(info.This());
 
