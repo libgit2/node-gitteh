33ae6500bcae6cc3b985ca0617c7874c1410e662
Sam Day <sam.c.day@gmail.com>
2011-03-02 09:57:27 +1000

    Added some repo tests.

---
+++ test/repository.test.js
@@ -0,0 +1,34 @@
+var vows = require("vows"),
+	assert = require("assert"),
+	gitteh = require("../build/default/gitteh"),
+	path = require("path"),
+	profiler = require("profiler"),
+	fixtureValues = require("./fixtures/values");
+
+vows.describe("Repository").addBatch({
+	"Opening an existing bare repository": {
+		topic: function() {
+			var repo = new gitteh.Repository(fixtureValues.REPO_PATH);
+			this.context.repo = repo;
+			
+			return repo;
+		},
+		
+		"opens correctly": function(repo) {
+			assert.instanceOf(repo, gitteh.Repository);
+		},
+		
+		"repo has correct path": function(repo) {
+			assert.equal(repo.path, fixtureValues.REPO_PATH);
+		},
+		
+		"repo path is immutable": function(repo) {
+			repo.path = "foo";
+			assert.equal(repo.path, fixtureValues.REPO_PATH);
+		},
+		
+		"Commits are not redundant": function(repo) {
+			assert.isTrue(repo.getCommit(fixtureValues.FIRST_COMMIT.id) === repo.getCommit(fixtureValues.FIRST_COMMIT.id));
+		}
+	}
+}).export(module);
