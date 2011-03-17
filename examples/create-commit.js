999b2b11fbb84660005ef6396f8b74beaca4dbab
Sam <sam.c.day@gmail.com>
2011-03-17 23:57:28 +1000

    Fix for create-commit egg-sample.

--- examples/create-commit.js
+++ examples/create-commit.js
@@ -2,7 +2,7 @@ var gitteh = require("gitteh"),
 	path = require("path"),
 	fs = require("fs");
 
-var repository = new gitteh.Repository(path.join(__dirname, "..", ".git"));
+var repository = gitteh.openRepository(path.join(__dirname, "..", ".git"));
 
 var newCommit = repository.createCommit();
 
