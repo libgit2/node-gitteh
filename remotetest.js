09d3019bfe7eb8d0cc45104b858ccd267c216cf4
samcday <sam.c.day@gmail.com>
2011-04-07 16:25:07 +1000

    Git protocol mostly implemented. Now I need to process packfiles - awesome -_-

--- remotetest.js
+++ remotetest.js
@@ -4,6 +4,8 @@ var path = require("path").join(__dirname, "remotetest");
 try {
 	require("wrench").rmdirSyncRecursive(path);
 }catch(e) {console.log(e); };
+
+/*
 //gitteh.Remote.clone("git://github.com/libgit2/node-gitteh.git", "/blah");
 
 gitteh.Remote.clone("https://github.com/libgit2/node-gitteh.git", path, function(err, result) {
@@ -12,4 +14,14 @@ gitteh.Remote.clone("https://github.com/libgit2/node-gitteh.git", path, function
 
 /*gitteh.Remote.clone("https://github.com/samcday/asyncevents.git", path, function(err, result) {
 	console.log(arguments);
+});*/
+
+
+gitteh.Remote.clone("git://github.com/libgit2/node-gitteh.git", path, function(err, repo) {
+	console.log("Clone result", arguments);
+});
+
+
+/*gitteh.Remote.clone("git://github.com/git/git.git", path, function(err, repo) {
+console.log("Clone result", arguments);
 });*/
