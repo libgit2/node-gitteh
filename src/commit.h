f42a831a5e78854d2f31c1557030060e7ffa66ed
Sam Day <sam.day@gmail.com>
2012-05-25 23:49:14 +1000

    cleanup continues: commit.h

--- src/commit.h
+++ src/commit.h
@@ -5,46 +5,17 @@
 #include "git_object.h"
 
 namespace gitteh {
-
-struct commit_data;
-class Repository;
-
-class Commit : public GitObject {
-public:
-	static Persistent<FunctionTemplate> constructor_template;
-	static void Init(Handle<Object>);
-	Commit(git_commit*);
-	~Commit();
-
-	static Handle<Value> SaveObject(Handle<Object>, Repository*, Handle<Value>, bool);
-
-	// void setOwner(Repository*);
-
-	Repository *repository_;
-	git_commit *commit_;
-
-protected:
-	static Handle<Value> New(const Arguments&);
-	/*static Handle<Value> GetTree(const Arguments&);
-	static Handle<Value> SetTree(const Arguments&);
-	static Handle<Value> AddParent(const Arguments&);
-	static Handle<Value> GetParent(const Arguments&);
-	static Handle<Value> Save(const Arguments&);
-
-	void processInitData();
-	int doInit();
-
-	int parentCount_;*/
-
-private:
-	/*static void EIO_Save(eio_req*);
-	static int EIO_AfterSave(eio_req*);
-
-	void updateCachedRef(const git_oid*);
-
-	commit_data *initData_;*/
-};
-
+	class Commit : public GitObject {
+	public:
+		static Persistent<FunctionTemplate> constructor_template;
+		static void Init(Handle<Object>);
+		Commit(git_commit*);
+		~Commit();
+	protected:
+		static Handle<Value> New(const Arguments&);
+	private:
+		git_commit *commit_;
+	};
 } // namespace gitteh
 
 #endif // GITTEH_COMMIT_H
